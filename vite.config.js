ac3ba207d4a2cfb5959fa10c00a774b1810f78c2
the-zahraa <zahraa.developer@gmail.com>
2026-06-19 16:52:56 +0330

    error

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,6 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-function getSiteUrl() {
-  const rawUrl =
-    process.env.VITE_SITE_URL ||
-    process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-    process.env.VERCEL_URL ||
-    'http://localhost:5173';
-
-  const urlWithProtocol = /^https?:\/\//.test(rawUrl) ? rawUrl : `https://${rawUrl}`;
-
-  return urlWithProtocol.replace(/\/+$/, '');
-}
-
-function airoSocialPreviewUrl() {
-  const siteUrl = getSiteUrl();
-
-  return {
-    name: 'airo-social-preview-url',
-    transformIndexHtml(html) {
-      return html.replace(/%AIRO_SITE_URL%/g, siteUrl);
-    }
-  };
-}
-
 export default defineConfig({
-  plugins: [svelte(), airoSocialPreviewUrl()]
+  plugins: [svelte()]
 });
